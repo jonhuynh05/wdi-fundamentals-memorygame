8e3798d579ba5c8b74b94198a760295980c05f8f
Jonathan Huynh <jonhuynh05@gmail.com>
2019-08-09 14:48:27 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,25 @@
-console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
+
+function checkForMath() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+
+cardsInPlay.push(cards[cardId]);
+
 if (cardsInPlay.length === 2){
+
+checkForMath()
+
+
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
 	}
@@ -13,3 +27,7 @@ if (cardsInPlay.length === 2){
 		alert("Sorry, try again.");
 	}
 }
+};
+
+flipCard(0);
+flipCard(2);
